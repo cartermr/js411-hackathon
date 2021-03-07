95b66ea8e79a5fa9856bc54ee2d62547ffc5f6a5
Matt Carter <carter.matthew@me.com>
2021-03-06 21:42:27 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useState } from 'react'
+
 function App() {
+  const [list, setList] = useState([])
+
+  const getList = (term, author, dateStart, dateEnd) => {
+    let URL = 'http://hn.algolia.com/api/v1/search_by_date?'
+
+    if (term !== '') {
+      URL = URL + `query=${term}`
+    }
+
+    if (author !== '' && URL !== 'http://hn.algolia.com/api/v1/search_by_date?') {
+      URL = URL + `&tags=author_${author}`
+    }
+
+    if (author !== '' && URL === 'http://hn.algolia.com/api/v1/search_by_date?') {
+      URL = URL + `tags=author_${author}`
+    }
+  }
+
   return (
     <div>
       
@@ -10,3 +30,5 @@ function App() {
 }
 
 export default App;
+
+
